679783ac716edd249816a3d69d01b561e66617fc
alwdis <alwdis@users.noreply.github.com>
2025-10-22 18:10:49 +0300

    Add IPC communication for auto-updater diagnostics in renderer process

--- public/preload.js
+++ public/preload.js
@@ -11,3 +11,34 @@ contextBridge.exposeInMainWorld('electronAPI', {
   getAvailableBackups: () => ipcRenderer.invoke('get-available-backups'),
   selectBackupFile: () => ipcRenderer.invoke('select-backup-file')
 });
+
+// Обработчик для диагностической информации
+ipcRenderer.on('debug-info', (event, data) => {
+  console.log('=== ELECTRON DEBUG INFO ===');
+  console.log('isDev:', data.isDev);
+  console.log('isPackaged:', data.isPackaged);
+  console.log('electronUpdaterAvailable:', data.electronUpdaterAvailable);
+  console.log('version:', data.version);
+  console.log('platform:', data.platform);
+  console.log('==========================');
+});
+
+// Обработчик для статуса автообновления
+ipcRenderer.on('auto-updater-status', (event, data) => {
+  console.log('=== AUTO-UPDATER STATUS ===');
+  console.log('isDev:', data.isDev);
+  console.log('forceAutoUpdate:', data.forceAutoUpdate);
+  console.log('autoUpdaterAvailable:', data.autoUpdaterAvailable);
+  console.log('shouldInitialize:', data.shouldInitialize);
+  console.log('==========================');
+});
+
+// Обработчик для событий автообновления
+ipcRenderer.on('auto-updater-event', (event, data) => {
+  console.log('=== AUTO-UPDATER EVENT ===');
+  console.log('Type:', data.type);
+  if (data.version) console.log('Version:', data.version);
+  if (data.error) console.log('Error:', data.error);
+  if (data.percent) console.log('Progress:', data.percent + '%');
+  console.log('==========================');
+});
