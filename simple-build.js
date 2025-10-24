3336492c6f3b85a16778d678a407ffad7a0af347
alwdis <alwdis@users.noreply.github.com>
2025-10-24 10:30:57 +0300

    Release v.5.0.12

--- simple-build.js
+++ simple-build.js
@@ -1,4 +1,1 @@
-// simple-build.js
-// No-op step to keep CI happy if you don't have a separate bundler.
-// If needed, you can add transpilation/minification here.
 console.log('simple-build: nothing to do');
